652bf9c5641d2a3538bbd5a1494ee42fd9c81894
Parshant Soni <parshantsoni2930@gmail.com>
2026-05-23 13:40:39 +0530

    final stable build

--- server/server.js
+++ server/server.js
@@ -24,7 +24,6 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
-
     return callback(new Error("Not allowed by CORS"));
   },
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
@@ -32,8 +31,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -79,6 +76,12 @@ io.on("connection", (socket) => {
   });
 });
 
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
 app.use((err, req, res, next) => {
   console.log("GLOBAL ERROR:", err);
 
@@ -91,12 +94,4 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION:", err);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION:", err);
 });
