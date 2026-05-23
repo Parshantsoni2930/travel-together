f332c8740bce179bbea637117cda836a59fc3b54
Parshant Soni <parshantsoni2930@gmail.com>
2026-05-23 13:46:01 +0530

    fix utils folder casing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
-const generateToken = require("../utils/generateToken");
+const generateToken = require("../Utils/generateToken");
 
 // REGISTER USER
 const registerUser = async (
